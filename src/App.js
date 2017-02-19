8419d139c1566ceb6d04e2a73421ac4271a936fe
Sarah Rose Battles <srosebattles@gmail.com>
2017-02-18 18:14:36 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ class App extends Component {
       <div className="App">
         <div className="navDiv">
           <ul className="nav">
-            <li className="navItem"><Link to={'/1'}>Level One: S A T I P</Link></li>
-            <li className="navItem"><Link to={'/2'}>Level Two: N CK E H R</Link></li>
-            <li className="navItem"><Link to={'/3'}>Level Three: M D G O U</Link></li>
-            <li className="navItem"><Link to={'/4'}>Level Four: L F B J Z</Link></li>
-            <li className="navItem"><Link to={'/5'}>Level Five: W V Y Q X</Link></li>
-            <li className="navItem"><Link to={'/6'}>Level Six: A E I O U</Link></li>
-            <li className="navItem"><Link to={'/7'}>Level Seven: B D P Q</Link></li>
-            <li className="navItem"><Link to={'/8'}>Level Eight: SH CH TH</Link></li>
-            <li className="navItem"><Link to={'/9'}>Level Nine: AI EE IE OO OU UE</Link></li>
-            <li className="navItem"><Link to={'/10'}>Level Ten: AR OR ER</Link></li>
+            <h1>Levels</h1>
+            <br />
+            <li className="navItem"><Link to={'/1'}>One: S A T I P</Link></li>
+            <li className="navItem"><Link to={'/2'}>Two: N CK E H R</Link></li>
+            <li className="navItem"><Link to={'/3'}>Three: M D G O U</Link></li>
+            <li className="navItem"><Link to={'/4'}>Four: L F B J Z</Link></li>
+            <li className="navItem"><Link to={'/5'}>Five: W V Y Q X</Link></li>
+            <li className="navItem"><Link to={'/6'}>Six: A E I O U</Link></li>
+            <li className="navItem"><Link to={'/7'}>Seven: B D P Q</Link></li>
+            <li className="navItem"><Link to={'/8'}>Eight: SH CH TH</Link></li>
+            <li className="navItem"><Link to={'/9'}>Nine: AI EE IE OO OU UE</Link></li>
+            <li className="navItem"><Link to={'/10'}>Ten: AR OR ER</Link></li>
           </ul>
         </div>
         <div className="interactDiv">
