312735241c4f26877addfec4656ea6a3a6855b28
Sarah Rose Battles <srosebattles@gmail.com>
2017-02-20 18:08:50 -0600

    add benchmark audio

--- src/LevelFour.js
+++ src/LevelFour.js
@@ -36,6 +36,21 @@ export default class LevelFour extends Component {
         <audio id="tryAgain">
           <source src="http://phonicsaudiofiles.s3.amazonaws.com/tryagain.mp3" type="audio/mpeg" />
         </audio>
+        <audio id="tenRight">
+          <source src="http://phonicsaudiofiles.s3.amazonaws.com/10right.mp3" type="audio/mpeg" />
+        </audio>
+        <audio id="twentyRight">
+          <source src="http://phonicsaudiofiles.s3.amazonaws.com/20points.mp3" type="audio/mpeg" />
+        </audio>
+        <audio id="thirtyRight">
+          <source src="http://phonicsaudiofiles.s3.amazonaws.com/30right.mp3" type="audio/mpeg" />
+        </audio>
+        <audio id="fortyRight">
+          <source src="http://phonicsaudiofiles.s3.amazonaws.com/40right.mp3" type="audio/mpeg" />
+        </audio>
+        <audio id="fiftyRight">
+          <source src="http://phonicsaudiofiles.s3.amazonaws.com/50points.mp3" type="audio/mpeg" />
+        </audio>
         <AudioPlayer src={this.state.soundLink}></AudioPlayer>
         <br/>
         <ContainerTwo nextSound={this.nextSound.bind(this)} answerShouldBe={this.state.answerShouldBe} />
